7e7452f694fe52e5de1f98b296a6a74a94d5b44c
mchlsdrv <michael@nanoscout.com>
2023-07-17 14:23:54 +0000

    train loop working

--- model.py
+++ model.py
@@ -11,21 +11,6 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 
-# - Hyperparameters
-# DATA_PATH = pathlib.Path('C:/Users/Michael/Desktop/University/PhD/Projects/CancerDet/Cancer Dataset')  # Windows
-DATA_PATH = pathlib.Path('/Users/mchlsdrv/Desktop/University/PhD/Projects/CancerDet/data')  # Mac
-METADATA_FILE = DATA_PATH / 'Rambam clinical table 26.6.23.csv'
-OUTPUT_DIR = pathlib.Path('/Users/mchlsdrv/Desktop/University/PhD/Projects/CancerDet/output')
-DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-HEIGHT = 512
-WIDTH = 512
-N_WORKERS = 4
-PIN_MEMORY = True
-BATCH_SIZE = 16
-VAL_BATCH_SIZE_SCALE = 16
-VAL_PROP = 0.2
-
 
 # - Classes
 class ResBlock(nn.Module):
@@ -155,14 +140,9 @@ def test_resnet152():
     print(y.shape)
 
 
-test_resnet50()
-test_resnet101()
-test_resnet152()
-
-
 class DataSet(Dataset):
-    def __init__(self, data: pd.DataFrame, augs: A.Compose):
-        self.data_df = data
+    def __init__(self, data_df: pd.DataFrame, augs: A.Compose):
+        self.data_df = data_df
         self.augs = augs
         # self.resize = A.Resize(height=HEIGHT, width=WIDTH, p=1.0)
         self.random_crop = A.RandomCrop(height=HEIGHT, width=WIDTH, p=1.0)
@@ -180,18 +160,14 @@ class DataSet(Dataset):
         # - Get the label
         lbl = np.array([pdl1, pdl2])
 
-        # print(scale)
-        # if scale <= 2:
-        #     # - If the image is zoomed-out - resize it to be able to fit into the network as is
-        #     img = self.resize(image=img, mask=img).get('image')
-        # else:
-        #     # - If the image is zoomed in more than x2 - crop a random crop
-        #     img = self.random_crop(image=img, mask=img).get('image')
-
         # - Run regular augmentations on the cropped / rescaled image
         img = self.augs(image=img, mask=img).get('image')
 
-        return torch.tensor(img, dtype=torch.float), torch.tensor(lbl, dtype=torch.float)
+        img, lbl = torch.tensor(img, dtype=torch.float), torch.tensor(lbl, dtype=torch.float)
+
+        img = torch.permute(img, (2, 0, 1))
+
+        return img, lbl
 
 
 # - Util functions
@@ -253,8 +229,8 @@ def get_test_augs():
         ], p=1.0)
 
 
-def get_train_val_split(data: pd.DataFrame, val_prop: float = .2):
-    n_items = len(data)
+def get_train_val_split(data_df: pd.DataFrame, val_prop: float = .2):
+    n_items = len(data_df)
     item_idxs = np.arange(n_items)
     n_val_items = int(n_items * val_prop)
 
@@ -262,22 +238,22 @@ def get_train_val_split(data: pd.DataFrame, val_prop: float = .2):
     val_idxs = np.random.choice(item_idxs, n_val_items, replace=False)
 
     # - Pick the items for the validation set
-    val_data = data.loc[val_idxs, :].reset_index(drop=True)
+    val_data = data_df.loc[val_idxs, :].reset_index(drop=True)
 
     # - The items for training are the once which are not included in the
     # validation set
-    train_data = data.loc[np.setdiff1d(item_idxs, val_idxs), :].reset_index(drop=True)
+    train_data = data_df.loc[np.setdiff1d(item_idxs, val_idxs), :].reset_index(drop=True)
 
     return train_data, val_data
 
 
 def get_data_loaders(data: pd.DataFrame, batch_size: int, val_prop: float):
     # - Split data into train / validation datasets
-    train_data, val_data = get_train_val_split(data=data, val_prop=val_prop)
+    train_data_df, val_data_df = get_train_val_split(data_df=data, val_prop=val_prop)
 
     # - Create the train / validation dataloaders
     train_dl = DataLoader(
-        DataSet(data=train_data, augs=get_train_augs()),
+        DataSet(data_df=train_data_df, augs=get_train_augs()),
         batch_size=batch_size,
         num_workers=N_WORKERS,
         pin_memory=PIN_MEMORY,
@@ -285,7 +261,7 @@ def get_data_loaders(data: pd.DataFrame, batch_size: int, val_prop: float):
     )
 
     val_dl = DataLoader(
-        DataSet(data=val_data, augs=get_test_augs()),
+        DataSet(data_df=val_data_df, augs=get_test_augs()),
         batch_size=batch_size // VAL_BATCH_SIZE_SCALE,
         num_workers=N_WORKERS,
         pin_memory=PIN_MEMORY,
@@ -303,7 +279,8 @@ def get_data(data_root_dir: pathlib.Path or str, metadata_file: pathlib.Path or 
     for root, folders, _ in os.walk(data_root_dir, topdown=False):
         for folder in folders:
             for sub_root, sub_folders, files in os.walk(pathlib.Path(root) / folder):
-                print(f'> Getting data from {sub_root} ...\n')
+                if DEBUG:
+                    print(f'> Getting data from {sub_root} ...\n')
                 for file in tqdm(files):
                     file_name, file_scale, file_index, file_type = get_name_scale_index_type(data_file=file)
                     values = lbls_df.loc[lbls_df.loc[:, 'Path number'] == file_name, ['PDL1 score', 'PDL2 score']]. \
@@ -311,12 +288,13 @@ def get_data(data_root_dir: pathlib.Path or str, metadata_file: pathlib.Path or 
                         flatten()
                     if len(values) > 1:
                         pdl1, pdl2 = values
-                        print(f'''
-                        File name: {file_name}/{file_index}
-                        - File scale: {file_scale}
-                        - PDL1 score: {pdl1}
-                        - PDL2 score: {pdl2}
-                        ''')
+                        if DEBUG:
+                            print(f'''
+                            File name: {file_name}/{file_index}
+                            - File scale: {file_scale}
+                            - PDL1 score: {pdl1}
+                            - PDL2 score: {pdl2}
+                            ''')
                         file_data_df = pd.DataFrame(
                             dict(
                                 path=f'{sub_root}/{file}',
@@ -331,69 +309,101 @@ def get_data(data_root_dir: pathlib.Path or str, metadata_file: pathlib.Path or 
                         )
                         data_df = pd.concat([data_df, file_data_df], axis=0, ignore_index=True)
                     else:
-                        print(f'(WARNING) No PDL1 / PDL2 values were found for file {sub_root}/{file}!')
+                        if DEBUG:
+                            print(f'(WARNING) No PDL1 / PDL2 values were found for file {sub_root}/{file}!')
     return data_df
+
+
+def get_x_ticks(epoch):
+    x_ticks = np.arange(epoch + 1)
+    if 20 < epoch < 50:
+        x_ticks = np.arange(0, epoch, 5)
+    elif 50 < epoch < 100:
+        x_ticks = np.arange(0, epoch, 10)
+    elif 100 < epoch < 1000:
+        x_ticks = np.arange(0, epoch, 50)
+    elif 1000 < epoch < 10000:
+        x_ticks = np.arange(0, epoch, 500)
+
+    return x_ticks
+
+
+# - Hyperparameters
+# -- General
+DEBUG = False
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# -- Paths
+# DATA_PATH = pathlib.Path('C:/Users/Michael/Desktop/University/PhD/Projects/CancerDet/Cancer Dataset')  # Windows
+# DATA_PATH = pathlib.Path('/Users/mchlsdrv/Desktop/University/PhD/Projects/CancerDet/data')  # Mac
+DATA_PATH = pathlib.Path('/home/sidorov/projects/cancer_det/data')  # 4GPUs
+METADATA_FILE = DATA_PATH / 'Rambam clinical table 26.6.23.csv'
+OUTPUT_DIR = pathlib.Path('/media/oldrrtammyfs/Users/sidorov/CancerDet/output')
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+# -- Architecture
+HEIGHT = 512
+WIDTH = 512
+CHANNELS = 3
+CLASSES = 2
+MODEL = get_resnet50(image_channels=CHANNELS, num_classes=CLASSES).to(DEVICE)
+
+# -- Training
+BATCH_SIZE = 16
+VAL_BATCH_SIZE_SCALE = 4
+VAL_PROP = 0.2
+LEARNING_RATE = 0.001
+EPOCHS = 100
+N_WORKERS = 4
+PIN_MEMORY = True
+
+OPTIMIZER = torch.optim.Adam(MODEL.parameters(), lr=LEARNING_RATE)
+LOSS_FUNC = nn.MSELoss()
 
 
 # - Main
 if __name__ == '__main__':
     print(f'- Getting data')
-    data_df = get_data(data_root_dir=DATA_PATH, metadata_file=METADATA_FILE)
-    print(data_df)
+    train_data_frame = get_data(data_root_dir=DATA_PATH, metadata_file=METADATA_FILE)
 
-    print(f'- Getting sample batches')
-    train_dl, val_dl = get_data_loaders(data=data_df, batch_size=BATCH_SIZE, val_prop=VAL_PROP)
-    train_imgs, train_lbls = next(iter(train_dl))
-    val_imgs, val_lbls = next(iter(val_dl))
-    print(f'''
-    - type(train_imgs): {type(train_imgs)}
-    - train_lbls.shape: {train_lbls.shape}
-    ''')
-    print(f'''
-    - type(val_imgs): {type(val_imgs)}
-    - val_lbls.shape: {val_lbls.shape}
-    ''')
-
-    # - Hyperparameters
-    learning_rate = 0.001
-    epochs = 10
-
-    model = get_resnet50(image_channels=3, num_classes=2).to(DEVICE)
-    opt = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    loss_func = nn.MSELoss()
+    train_data_loader, val_data_loader = get_data_loaders(
+        data=train_data_frame,
+        batch_size=BATCH_SIZE,
+        val_prop=VAL_PROP
+    )
 
     epoch_train_losses = np.array([])
     epoch_val_losses = np.array([])
     # - Training loop
-    for epoch in tqdm(range(epochs)):
+    for epch in tqdm(range(EPOCHS)):
         # - Train
         btch_train_losses = np.array([])
         btch_val_losses = np.array([])
-        for btch_idx, (imgs, lbls) in enumerate(train_dl):
+        for btch_idx, (imgs, lbls) in enumerate(train_data_loader):
             # - Store the data in CUDA
             imgs = imgs.to(DEVICE)
             lbls = lbls.to(DEVICE)
 
             # - Forward
-            preds = model(imgs)
-            loss = loss_func(preds, lbls)
+            preds = MODEL(imgs)
+            loss = LOSS_FUNC(preds, lbls)
             btch_train_losses = np.append(btch_train_losses, loss.item())
 
             # - Backward
-            opt.zero_grad()
+            OPTIMIZER.zero_grad()
             loss.backward()
 
             # - Optimizer step
-            opt.step()
+            OPTIMIZER.step()
 
         # - Validation
         with torch.no_grad():
-            for btch_idx, (imgs, lbls) in enumerate(val_dl):
+            for btch_idx, (imgs, lbls) in enumerate(val_data_loader):
                 imgs = imgs.to(DEVICE)
                 lbls = lbls.to(DEVICE)
 
-                preds = model(imgs)
-                loss = loss_func(preds, lbls)
+                preds = MODEL(imgs)
+                loss = LOSS_FUNC(preds, lbls)
 
                 btch_val_losses = np.append(btch_val_losses, loss.item())
 
@@ -401,10 +411,14 @@ if __name__ == '__main__':
         epoch_val_losses = np.append(epoch_val_losses, btch_val_losses.mean())
 
         fig, ax = plt.subplots()
-        ax.plot(np.arange(epoch + 1), epoch_train_losses, label='train')
-        ax.plot(np.arange(epoch + 1), epoch_val_losses, label='val')
-        ax.set(xlabel='Epoch', ylabel='MSE')
+        epochs = np.arange(epch + 1)
+
+        ax.plot(epochs, epoch_train_losses, 'bo-', label='train')
+        ax.plot(epochs, epoch_val_losses, 'r*-', label='val')
+        ax.set(xlabel='Epoch', ylabel='MSE', xticks=get_x_ticks(epoch=epch))#, xlim=[0, epch])
         fig.suptitle('Loss vs Epochs')
-        plt.legend(True)
-        plt.savefig(OUTPUT_DIR / 'loss.png')
+        plt.legend()
+        plot_output_dir = OUTPUT_DIR / 'plots'
+        os.makedirs(plot_output_dir, exist_ok=True)
+        fig.savefig(plot_output_dir / 'loss.png')
         plt.close(fig)
