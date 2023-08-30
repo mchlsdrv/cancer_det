8162de6d76ad2f54e7c9a63b6f24950f41e01bc2
mchlsdrv <michael@nanoscout.com>
2023-08-30 11:46:13 +0000

    added regression model - working script

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ from ml.nn.utils.aux_funcs import (
     get_train_val_split,
     load_checkpoint,
     get_arg_parser,
-    get_device, train, get_data_loaders_from_datasets
+    get_device, train, get_data_loaders_from_datasets, save_images, plot_scatter
 )
 from python_utils.image_utils import get_image
 
@@ -29,6 +29,8 @@ plt.style.use('ggplot')
 # - Constants
 EPSILON = 1e-9
 BETA = 1.0
+ERROR_THRESHOLD = 0.1
+
 
 TS = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 PARSER = get_arg_parser()
@@ -56,13 +58,14 @@ if isinstance(ARGS.name, str):
 else:
     OUTPUT_DIR = pathlib.Path(f'/media/oldrrtammyfs/Users/sidorov/CancerDet/output/{TS}')
 os.makedirs(OUTPUT_DIR, exist_ok=True)
-CHECKPOINT_FILE = '/media/oldrrtammyfs/Users/sidorov/CancerDet/output/CELoss_train_no_stoch_depth_gray_50k_2023-08-25_07-46-10/checkpoints/weights_epoch_49.pth.tar'
-# CHECKPOINT_FILE = '/media/oldrrtammyfs/Users/sidorov/CancerDet/output/CELoss_train_test_stoch_depth_rgb_2023-08-23_07-22-36/checkpoints/weights_epoch_99.pth.tar'
+CHECKPOINT_FILE = '/media/oldrrtammyfs/Users/sidorov/CancerDet/output/MSELoss_train_all_data_cont_2023-08-28_13-56-46/checkpoints/weights_epoch_99.pth.tar'
+# CHECKPOINT_FILE = '/media/oldrrtammyfs/Users/sidorov/CancerDet/output/CELoss_train_no_stoch_depth_gray_all_data_2023-08-25_23-27-22/checkpoints/weights_epoch_89.pth.tar'
+# CHECKPOINT_FILE = '/media/oldrrtammyfs/Users/sidorov/CancerDet/output/CELoss_train_no_stoch_depth_gray_50k_2023-08-25_07-46-10/checkpoints/weights_epoch_49.pth.tar'
 # -- Architecture
 HEIGHT = 256
 WIDTH = 256
 # CHANNELS = 3
-CHANNELS = 1
+CHANNELS = 3 if ARGS.rgb else 1
 OUTPUT_SIZE = 2
 
 # -- Training
@@ -73,25 +76,31 @@ TRAIN_BATCH_SIZE = ARGS.batch_size
 VAL_BATCH_SIZE = TRAIN_BATCH_SIZE // 2
 LEARNING_RATE = ARGS.learning_rate
 EPOCHS = ARGS.epochs
-TO_RGB = True if CHANNELS == 3 else False
-TO_GRAY = True if CHANNELS == 1 else False
+TO_RGB = True if ARGS.rgb else False
+TO_GRAY = True if not ARGS.rgb else False
 VAL_PROP = 0.2
 N_WORKERS = 4
 PIN_MEMORY = True
 CHANNELS_FIRST = True
 
 # > Loss
-ONE_HOT_LABELS = True
-CLASSIFICATION_MODEL = True
+# * Choosing the model - binary classification or regression
+if ARGS.model_type == 'bin':
+    CLASSIFICATION_MODEL = True
+    REGRESSION_MODEL = False
+else:
+    REGRESSION_MODEL = True
+    CLASSIFICATION_MODEL = False
+
 if CLASSIFICATION_MODEL:
-    LOSS_FUNC = nn.CrossEntropyLoss() if ONE_HOT_LABELS else nn.BCELoss()
+    LOSS_FUNC = nn.CrossEntropyLoss()
 else:
     LOSS_FUNC = nn.MSELoss()
 
 MODEL = get_resnet50(
     image_channels=CHANNELS,
     output_size=OUTPUT_SIZE,
-    prediction_layer=None if ONE_HOT_LABELS else nn.Sigmoid(),
+    prediction_layer=None,
 ).to(DEVICE)
 
 # > Optimizer
@@ -104,12 +113,12 @@ LOSS_PLOT_RESOLUTION = 10
 
 class DataSet(Dataset):
     def __init__(self, data_df: pd.DataFrame, augs: A.Compose, to_gray: bool,
-                 classification_model: bool = True, channels_first: bool = False):
+                 model_type: str, channels_first: bool = False):
         self.data_df = data_df
         self.augs = augs
         self.random_crop = A.RandomCrop(height=HEIGHT, width=WIDTH, p=1.0)
         self.to_gray = to_gray
-        self.classification_model = classification_model
+        self.model_type = model_type
         self.channels_first = channels_first
 
     def __len__(self):
@@ -125,25 +134,17 @@ class DataSet(Dataset):
         img = get_image(image_file=img_fl, to_gray=self.to_gray)
 
         # - Get the label
-        if self.classification_model:
-            # # - Use one-hot labels
-            # if self.one_hot_labels:
-            #     if rspns > 0:
-            #         lbl = np.array([1., 0.])
-            #     else:
-            #         lbl = np.array([0., 1.])
-            # # - Use continuous label in 0 or 1
-            # else:
-            lbl = rspns
-            # lbl = np.array([rspns])
         # - Use continuous labels representing the values for regression model
+        if self.model_type == 'reg':
+            lbl = torch.tensor(np.array([pdl1, pdl2]), dtype=torch.float)
         else:
-            lbl = np.array([pdl1, pdl2])
+            lbl = torch.tensor(np.array(rspns), dtype=torch.long)
 
         # - Run regular augmentations on the cropped / rescaled image
         img = self.augs(image=img, mask=img).get('image')
 
-        img, lbl = torch.tensor(img, dtype=torch.float), torch.tensor(lbl, dtype=torch.long)
+        # - Convert image to tensor
+        img = torch.tensor(img, dtype=torch.float)
 
         if self.channels_first:
             img = torch.permute(img, (2, 0, 1))  # Add channels dim before width and height
@@ -190,7 +191,8 @@ def get_test_augs():
         ], p=1.0)
 
 
-def get_data_from_dir(data_root_dir: pathlib.Path or str, metadata_file: pathlib.Path or str, save_file: pathlib.Path or str):
+def get_data_from_dir(data_root_dir: pathlib.Path or str, metadata_file: pathlib.Path or str,
+                      save_file: pathlib.Path or str):
     metadata_df = pd.read_csv(metadata_file)
     lbls_df = metadata_df.loc[7:35, ['Path number', 'PD1 score', 'PDL1 score', 'PDL2 score', 'Response binary']]
 
@@ -262,7 +264,7 @@ def get_data_loaders():
         data_df=train_df,
         augs=get_train_augs(),
         to_gray=TO_GRAY,
-        classification_model=CLASSIFICATION_MODEL,
+        model_type=ARGS.model_type,
         channels_first=CHANNELS_FIRST
     )
 
@@ -270,7 +272,7 @@ def get_data_loaders():
         data_df=val_df,
         augs=get_test_augs(),
         to_gray=TO_GRAY,
-        classification_model=CLASSIFICATION_MODEL,
+        model_type=ARGS.model_type,
         channels_first=CHANNELS_FIRST
     )
 
@@ -283,15 +285,19 @@ def get_data_loaders():
     return train_data_loader, val_data_loader
 
 
-def predict(image_files):
+def predict_label(image_files, continuous: bool = False):
     """
     Returns a 1D vector of length len(images) with float values representing the scores in range [0., 1.] for each image
     :param image_files: Images to be predicted
+    :param continuous: If the label should be continuous or binary
     :return: Predictions (1D float vector)
     """
     # - Put the model in evaluation mode
     MODEL.eval()
+    imgs = []
+    img_nms = []
     preds = []
+    print(f'- Predicting images')
     for img_fl in tqdm(image_files):
         img = get_image(
             image_file=img_fl,
@@ -302,23 +308,63 @@ def predict(image_files):
             device=DEVICE
         )
 
-        cls_probs = MODEL(img)
-        _, pred = torch.max(cls_probs, 1)  # returns (values, indices), but as our class is the same as the index,
-        # we may ignore the raw value
+        logits = MODEL(img)
+        if continuous:
+            pred = logits
+        else:
+            _, pred = torch.max(logits, 1)  # returns (values, indices), but as our class is the same as the index,
+            # we may ignore the raw value
 
         # - Append the label to the labels
-        preds.append(pred.item())
+        if DEBUG:
+            img = get_image(
+                image_file=img_fl,
+                to_gray=TO_GRAY,
+                add_channel_dim=False
+            )
+            imgs.append(img)
+            img_nm = pathlib.Path(img_fl).name
+            img_nms.append(img_nm)
+        preds.append(pred.cpu().detach().numpy()[0])
 
     # - Put the model back in train mode
     MODEL.train()
 
-    return np.array(preds)
+    return np.array(preds), np.array(imgs), np.array(img_nms)
 
 
-def test():
+def test_binary_classification():
     test_df = pd.read_csv(TEST_DATA_FRAME)
     image_files, labels = test_df.loc[:, 'path'].values, test_df.loc[:, 'response'].values
-    preds = predict(image_files=image_files)
+    preds, imgs, img_nms = predict_label(image_files=image_files, continuous=False)
+
+    print(f'- Testing {len(image_files)} images')
+    # - Monitor the false positive / negative samples
+    if DEBUG:
+
+        # - False positives are 1 while they should be 0, so to get to them, we reverse the label and multipy by
+        # predictions e.g.,
+        # > pred                   [1, 0, 0, 1, 1]
+        # > label                  [0, 0, 1, 0, 1]
+        # > inv(label)             [1, 1, 0, 1, 0]
+        # => pred * inv(label)     [1, 0, 0, 1, 0]
+        fp = preds * np.logical_not(labels)
+        fp_idxs = np.argwhere(fp > 0).flatten()
+        fp_imgs = imgs[fp_idxs]
+        fp_img_nms = img_nms[fp_idxs]
+        save_images(fp_imgs, image_names=fp_img_nms, save_dir=OUTPUT_DIR / 'errors/FP')
+
+        # - False negatives are 0 while they should be 1, so to get to them, we reverse the predictions and multipy by
+        # labels e.g.,
+        # > pred                   [1, 0, 0, 1, 1]
+        # > inv(pred)              [0, 1, 1, 0, 0]
+        # > label                  [0, 0, 1, 0, 1]
+        # => inv(pred) * label     [0, 0, 1, 0, 0]
+        fn = np.logical_not(preds) * labels
+        fn_idxs = np.argwhere(fn > 0).flatten()
+        fn_imgs = imgs[fn_idxs]
+        fn_img_nms = img_nms[fn_idxs]
+        save_images(fn_imgs, image_names=fn_img_nms, save_dir=OUTPUT_DIR / 'errors/FN')
 
     prcsn, rcll, f1_scr, _ = sklearn.metrics.precision_recall_fscore_support(preds, labels, beta=BETA, average='binary')
 
@@ -328,6 +374,35 @@ def test():
     conf_mat_disp.plot()
 
     return prcsn, rcll, f1_scr, conf_mat_disp.figure_
+
+
+# noinspection PyShadowingNames
+def test_continuous_label():
+    test_df = pd.read_csv(TEST_DATA_FRAME)
+    image_files, labels_pdl1, labels_pdl2 = test_df.loc[:, 'path'].values, test_df.loc[:, 'pdl1'].values, test_df.loc[:, 'pdl2'].values
+    preds, imgs, img_nms = predict_label(image_files=image_files, continuous=True)
+    preds_pdl1, preds_pdl2 = preds[:, 0], preds[:, 1]
+
+    print(f'- Testing {len(image_files)} images')
+    # - Monitor the large error samples
+    if DEBUG:
+        # - PDL1 errors
+        pdl1_sq_errs = np.sqrt(preds_pdl1 - labels_pdl1)
+        large_errs_idxs = np.argwhere(pdl1_sq_errs > ERROR_THRESHOLD).flatten()
+        err_imgs = imgs[large_errs_idxs]
+        err_img_nms = img_nms[large_errs_idxs]
+        save_images(err_imgs, image_names=err_img_nms, squared_errors=pdl1_sq_errs, save_dir=OUTPUT_DIR / 'pdl1_errors')
+
+        # - PDL2 errors
+        pdl2_sq_errs = np.sqrt(preds_pdl2 - labels_pdl2)
+        large_errs_idxs = np.argwhere(pdl2_sq_errs > ERROR_THRESHOLD).flatten()
+        err_imgs = imgs[large_errs_idxs]
+        err_img_nms = img_nms[large_errs_idxs]
+        save_images(err_imgs, image_names=err_img_nms, squared_errors=pdl2_sq_errs, save_dir=OUTPUT_DIR / 'pdl2_errors')
+
+    scatter_fig = plot_scatter(true=[labels_pdl1, labels_pdl2], predicted=[preds_pdl1, preds_pdl2], labels=['pl1', 'pl2'])
+
+    return scatter_fig
 
 
 if __name__ == '__main__':
@@ -344,18 +419,23 @@ if __name__ == '__main__':
         train(model=MODEL, train_data_loader=train_dl, val_data_loader=val_dl, optimizer=OPTIMIZER, loss_func=LOSS_FUNC,
               epochs=EPOCHS, device=DEVICE, output_dir=OUTPUT_DIR)
 
-    precision, recall, f1_score, conf_mat_fig = test()
+    elif ARGS.test:
+        if CLASSIFICATION_MODEL:
+            precision, recall, f1_score, conf_mat_fig = test_binary_classification()
 
-    print(f'''
-    TEST RESULTS:
-        - Precision (TP / (TP + FP)): {precision:.3f}
-        - Recall (TP / (TP + FN)): {recall:.3f}
-        - F1 Score (2 * (Precision * Recall) / (Precision + Recall)): {f1_score:.3f}
-    ''')
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    plot_output_dir = OUTPUT_DIR / 'plots'
-    os.makedirs(plot_output_dir, exist_ok=True)
-    conf_mat_fig.savefig(plot_output_dir / 'confusion matrix.png')
-    plt.close(conf_mat_fig)
-
-
+            print(f'''
+            TEST RESULTS:
+                - Precision (TP / (TP + FP)): {precision:.3f}
+                - Recall (TP / (TP + FN)): {recall:.3f}
+                - F1 Score (2 * (Precision * Recall) / (Precision + Recall)): {f1_score:.3f}
+            ''')
+            plot_output_dir = OUTPUT_DIR / 'plots'
+            os.makedirs(plot_output_dir, exist_ok=True)
+            conf_mat_fig.savefig(plot_output_dir / 'confusion matrix.png')
+            plt.close(conf_mat_fig)
+        elif REGRESSION_MODEL:
+            scatter_fig = test_continuous_label()
+            plot_output_dir = OUTPUT_DIR / 'plots'
+            os.makedirs(plot_output_dir, exist_ok=True)
+            scatter_fig.savefig(plot_output_dir / 'scatter plot.png')
+            plt.close(scatter_fig)
